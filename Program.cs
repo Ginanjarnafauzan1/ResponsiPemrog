74bc9aa51fd6c9d838149d3fb37c2773700a3316
Ginanjar Nafauzan <Anggurunggar@gmail.com>
2019-05-03 20:23:29 +0700

    responsi

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2258
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan();
+
+            kry1.Nama = "Ginanjar";
+            kry1.Nik = "18.11.2258";
+            kry1.GajiBulanan = 5000000;
+
+            Karyawan kry2 = new Karyawan();
+
+            kry2.Nama = "Nafauzan";
+            kry2.Nik = "18.11.2256";
+            kry2.GajiBulanan = 4500000;
+
+            Console.WriteLine("no.\tNik/Nama\t\tGajiBulanan");
+            Console.WriteLine("============================");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan);
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan);
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Gaji Naik 10%");
+            Console.WriteLine("\n");
+            Console.WriteLine("no.\tNik/Nama\t\tGajiBulanan");
+            Console.WriteLine("=============================");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan + (kry1.GajiBulanan * 0.10));
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan + (kry2.GajiBulanan * 0.10));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResponsiPemrog2258")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("ResponsiPemrog2258")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d04d749d-5d72-44b2-9fd0-c9cf1c55ccbf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
